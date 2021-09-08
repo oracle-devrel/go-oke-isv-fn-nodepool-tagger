bfbe7a5b0fdfe82b6a11560480af1715fc792c16
Ed Shnekendorf <eshneken@gmail.com>
2021-09-08 11:47:54 -0400

    fixed spacing problems in comments

--- function/cloudevent.go
+++ function/cloudevent.go
@@ -1,4 +1,4 @@
-//	ISV Serverless Nodepool Tagger, Ed Shnekendorf, September 2021
+//  ISV Serverless Nodepool Tagger, Ed Shnekendorf, September 2021
 //  Copyright (c) 2021 Oracle and/or its affiliates.
 //  Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 package main
